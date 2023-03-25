faf6d31e114d8e48b90c307663ddfbf842452c11
Matheus Assis <math.assis91@gmail.com>
2023-03-25 13:02:50 -0300

    Colecionaveis e Puzzle

--- Adventure Game/Assets/AA-PROJETO/Scripts/Itens/ItensMenuColecionavel.cs
+++ Adventure Game/Assets/AA-PROJETO/Scripts/Itens/ItensMenuColecionavel.cs
@@ -1,77 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItensMenuColecionavel : MonoBehaviour
 {
-    [SerializeField] private GameObject itemInfo, itemDica, item1, item2, item3, item4, item5;
-    public enum Opcoes {item1, item2, item3, item4, item5 };
+    [SerializeField] private Image[] item;
+    [SerializeField] private GameObject itemInfo, itemDica;
+    public enum Opcoes { item1, item2, item3, item4, item5 };
     public Opcoes opcoes;
+    private GlobalVars script;
 
+    private void Start()
+    {
+        script = FindObjectOfType<GlobalVars>();
+    }
 
-    
     private void Update()
     {
-       /* switch(opcoes)
+        switch (opcoes)
         {
             case (Opcoes.item1):
-                if(script.colecionaveis == 0)
+                if (script.colecionaveis == 0)
                 {
                     itemInfo.SetActive(true);
                     itemDica.SetActive(false);
+                    var tempColor = item[0].color;
+                    tempColor.a = 1f;
+                    item[0].color = tempColor;
                 }
                 break;
 
             case (Opcoes.item2):
-                if (script.itemDois.Equals(true))
+                if (script.colecionaveis == 1)
                 {
                     itemInfo.SetActive(true);
                     itemDica.SetActive(false);
-                }
-                else
-                {
-                    itemInfo.SetActive(false);
-                    itemDica.SetActive(true);
+                    var tempColor = item[1].color;
+                    tempColor.a = 1f;
+                    item[1].color = tempColor;
                 }
                 break;
 
             case (Opcoes.item3):
-                if (script.itemDois.Equals(true))
+                if (script.colecionaveis == 2)
                 {
                     itemInfo.SetActive(true);
                     itemDica.SetActive(false);
-                }
-                else
-                {
-                    itemInfo.SetActive(false);
-                    itemDica.SetActive(true);
+                    var tempColor = item[2].color;
+                    tempColor.a = 1f;
+                    item[2].color = tempColor;
                 }
                 break;
 
             case (Opcoes.item4):
-                if (script.itemDois.Equals(true))
+                if (script.colecionaveis == 3)
                 {
                     itemInfo.SetActive(true);
                     itemDica.SetActive(false);
-                }
-                else
-                {
-                    itemInfo.SetActive(false);
-                    itemDica.SetActive(true);
+                    var tempColor = item[3].color;
+                    tempColor.a = 1f;
+                    item[3].color = tempColor;
                 }
                 break;
 
             case (Opcoes.item5):
-                if (script.itemDois.Equals(true))
+                if (script.colecionaveis == 4)
                 {
                     itemInfo.SetActive(true);
                     itemDica.SetActive(false);
+                    var tempColor = item[4].color;
+                    tempColor.a = 1f;
+                    item[4].color = tempColor;
                 }
-                else
-                {
-                    itemInfo.SetActive(false);
-                    itemDica.SetActive(true);
-                }
-                break;*/
+                break;
+        }
+
+        if (script.colecionaveis == -1)
+        {
+            foreach (Image img in item)
+            {
+                var tempColor = img.color;
+                tempColor.a = 0.2f;
+                img.color = tempColor;
+            }
         }
     }
+}
--- Adventure Game/Assets/AA-PROJETO/Scripts/Itens/setasMenuColecionavel.cs
+++ Adventure Game/Assets/AA-PROJETO/Scripts/Itens/setasMenuColecionavel.cs
@@ -11,13 +11,13 @@ public class setasMenuColecionavel : MonoBehaviour
     {
         if(_index == 0)
         {
-            _index = 3;
-            colecionaveis[3].SetActive(true);
+            _index = 4;
+            colecionaveis[4].SetActive(true);
             colecionaveis[0].SetActive(false);
         }
         else
         {
-            _index = Mathf.Clamp(_index - 1, 0, 3);
+            _index = Mathf.Clamp(_index - 1, 0, 4);
             colecionaveis[_index].SetActive(true);
             colecionaveis[_index + 1].SetActive(false);
         }
@@ -25,15 +25,15 @@ public class setasMenuColecionavel : MonoBehaviour
 
     public void Direita()
     {
-        if (_index == 3)
+        if (_index == 4)
         {
             _index = 0;
             colecionaveis[0].SetActive(true);
-            colecionaveis[3].SetActive(false);
+            colecionaveis[4].SetActive(false);
         }
         else
         {
-            _index = Mathf.Clamp(_index + 1, 0, 3);
+            _index = Mathf.Clamp(_index + 1, 0, 4);
             colecionaveis[_index].SetActive(true);
             colecionaveis[_index - 1].SetActive(false);
         }
