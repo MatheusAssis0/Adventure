760da10b5894d0895857a8108561babd4b20b1fc
MatheusAssis0 <113194203+MatheusAssis0@users.noreply.github.com>
2023-03-27 17:29:11 -0300

    Build V 3.1

---
+++ Adventure Game/Assets/AA-PROJETO/Scripts/Floresta/InicioFloresta.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+public class InicioFloresta : MonoBehaviour
+{
+    [SerializeField] private Camera main;
+    [SerializeField] private GameObject canvas;
+    private PlayerMovement script;
+
+    private void Start()
+    {
+        script = FindObjectOfType<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if (canvas.activeSelf == true)
+        {
+            script.enabled = false;
+            script.anim.SetBool("Andando", false);
+        }
+        else if (canvas.activeSelf == false)
+        {
+            script.enabled = true;
+        }
+    }
+}
